92b899e97360183de42f818bb3d25ba406dde530
idoyudha <idowidya.yudhatama@gmail.com>
2021-01-26 03:58:17 +0700

    Update file for entry and index page

--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -1,10 +1,27 @@
 from django.shortcuts import render
 
 from . import util
-
+import markdown
 
 def index(request):
+    entries = util.list_entries()
     return render(request, "encyclopedia/index.html", {
-        "entries": util.list_entries()
+        "entries": entries
     })
 
+def entry(request, title):
+    data = util.get_entry(title)
+    filter = markdown.markdown(data)
+    context_entry = {
+        "filter": filter
+    }
+    context_none = {
+        "title": title
+    }
+    if data == "None":
+        return render(request, "encyclopedia/notexist.html", context_none)
+    else:
+        return render(request, "encyclopedia/entry.html", context_entry)
+
+def search(request):
+    pass
